9ced913d1c0ec3e8e6624e008dd80edcb94b137c
notAnUzver <voron.svoboda@gmail.com>
2016-05-15 01:06:47 +0400

    fixed backgrounds

--- public/JS/bground.js
+++ public/JS/bground.js
@@ -1,16 +1,16 @@
 ﻿var i = 0;
 function changeBground(){
 	switch(i){
-		case 0: document.getElementById('bground').style.background="url(/pics/bground/art1.jpg) no-repeat";
-                document.getElementById('bground').style.backgroundSize = "100%";
+        case 0: document.getElementById('bground').style.background = "url(/pics/bground/art1.jpg) no-repeat";
+                document.getElementById('bground').style.backgroundSize = "cover";
                 document.getElementById('bground').style.backgroundPosition = "center";
 				break;
         case 1: document.getElementById('bground').style.background = "url(/pics/bground/art2.jpg) no-repeat";
-                document.getElementById('bground').style.backgroundSize = "100%";
+                document.getElementById('bground').style.backgroundSize = "cover";
                 document.getElementById('bground').style.backgroundPosition = "center";
                 break;
-        case 2: document.getElementById('bground').style.background = "url(/pics/bground/art3.jpg) no-repeat";
-                document.getElementById('bground').style.backgroundSize = "100%";
+        case 2: document.getElementById('bground').style.background = "url(/pics/bground/art3.JPG) no-repeat";
+                document.getElementById('bground').style.backgroundSize = "cover";
                 document.getElementById('bground').style.backgroundPosition = "center";
                 break;
 	};
