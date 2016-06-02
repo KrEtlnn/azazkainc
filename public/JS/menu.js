353e23a135c6b1fa6edd44c1b21eb2ce763a316c
notAnUzver <voron.svoboda@gmail.com>
2016-06-02 14:11:04 +0400

    fixed menu

---
+++ public/JS/menu.js
@@ -0,0 +1,15 @@
+﻿window.onscroll = menu_scroll;
+
+function menu_scroll() {
+    if ($('html').width() > 1025) {
+        if (document.body.scrollTop > 54)
+            $('.menu').fadeIn("slow", function () {
+                $('.menu').css('position', 'fixed');
+                $('.menu').css('top', -10);
+            });
+        if (document.body.scrollTop < 54) {
+            $('.menu').css('position', 'relative');
+            $('.menu').css('top', 0);
+        }
+    }
+};
